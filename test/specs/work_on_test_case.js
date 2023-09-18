b067d58b74f2702304cdb5e63ea331eadf287ad3
VadymMakhobey <vadimmakhobey@gmail.com>
2023-09-18 14:46:17 +0300

    Test case 2

--- test/specs/work_on_test_case.js
+++ test/specs/work_on_test_case.js
@@ -7,15 +7,37 @@ describe('Webdriverio main page', () => {
     xit("should check that the pop-up window is showing", async () =>{
         await browser.url('https://github.com/');
         await browser.pause(2000)
-        const link = await $('[class="HeaderMenu-link border-0 width-full width-lg-auto px-0 px-lg-2 py-3 py-lg-2 no-wrap d-flex flex-items-center flex-justify-between js-details-target"]')
+        const link = await $('[class="HeaderMenu-link border-0 width-full width-lg-auto px-0 px-lg-2 py-3 py-lg-2 no-wrap d-flex flex-items-center flex-justify-between js-details-target"]');
         await link.moveTo()
         await browser.pause(2000)
-        let link1 = await $('[class="HeaderMenu-dropdown dropdown-menu rounded m-0 p-0 py-2 py-lg-4 position-relative position-lg-absolute left-0 left-lg-n3 d-lg-flex dropdown-menu-wide"]')
+        let link1 = await $('[class="HeaderMenu-dropdown dropdown-menu rounded m-0 p-0 py-2 py-lg-4 position-relative position-lg-absolute left-0 left-lg-n3 d-lg-flex dropdown-menu-wide"]');
         let check = await link1.isDisplayed()
         console.log("available: " + check)
         await browser.pause(1000)
         
 
+    });
+
+    xit("should show whether the text is displayed correctly", async () =>{
+        await browser.url('https://github.com/');
+        await browser.pause(2000)
+
+        let link = await $('[class="HeaderMenu-link HeaderMenu-link--sign-up flex-shrink-0 d-none d-lg-inline-block no-underline border color-border-default rounded px-2 py-1"]');
+        await link.click()
+
+        await browser.pause(5000)
+
+        let link1 = await $('h1');
+        let check = await link1.isDisplayed()
+        console.log("available: " + check)
+
+        await browser.pause(2000)
+
+        let text =  await link1.getText()
+        if(text = "Welcome to GitHub! Let's begin the adventure"){
+            console.log("text correct: " + text )
+        }
+        await browser.pause(2000)
     });
 
 });
